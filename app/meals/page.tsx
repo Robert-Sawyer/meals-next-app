7d0f7d9f93dd7fbacb890acc08b88ababec6bcc3
Robert Olbrychowski <47324286+Robert-Sawyer@users.noreply.github.com>
2026-05-09 17:55:48 +0200

    New files

---
+++ app/meals/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function MealsPage() {
+    return (
+        <div>
+            Meals page
+            <Link href="/meals/share">Go to Meals Share</Link>
+        </div>
+    )
+}
