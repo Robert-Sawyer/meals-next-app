2726f61d895870091b1798d926bbea706351ee60
Robert Olbrychowski <47324286+Robert-Sawyer@users.noreply.github.com>
2026-05-11 22:08:46 +0200

    Change handling loading data, add error page

--- app/meals/page.tsx
+++ app/meals/page.tsx
@@ -2,10 +2,15 @@ import classes from './page.module.css'
 import Link from "next/link";
 import MealsGrid from "../../components/Meals/MealsGrid";
 import {getMeals} from '../../lib/meals';
+import {Suspense} from "react";
 
-export default async function MealsPage() {
+async function Meals() {
     const meals = await getMeals();
 
+    return <MealsGrid meals={meals}/>
+}
+
+export default async function MealsPage() {
     return (
         <>
         <header className={classes.header}>
@@ -19,7 +24,9 @@ export default async function MealsPage() {
         </header>
 
             <main className={classes.main}>
-                <MealsGrid meals={meals}/>
+                <Suspense fallback={<p className={classes.loading}>Fetching meals...</p>}>
+                    <Meals/>
+                </Suspense>
             </main>
         </>
     )
