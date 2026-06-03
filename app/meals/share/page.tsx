5f386701300ebbfd339597b8b14d3204fff3af8e
Robert Olbrychowski <47324286+Robert-Sawyer@users.noreply.github.com>
2026-06-03 16:30:58 +0200

    Basic validation

--- app/meals/share/page.tsx
+++ app/meals/share/page.tsx
@@ -1,8 +1,14 @@
+'use client';
+
 import classes from './page.module.css';
 import ImagePicker from "../../../components/Meals/ImagePicker";
 import {shareMeal} from "../../../lib/actions";
+import {MealsFormSubmit} from "../../../components/Meals/MealsFormSubmit";
+import {useActionState} from "react";
 
 export default function ShareMealPage() {
+    const [state, formActions] = useActionState(shareMeal, {message: null})
+
     return (
         <>
             <header className={classes.header}>
@@ -13,24 +19,24 @@ export default function ShareMealPage() {
             </header>
 
             <main className={classes.main}>
-                <form className={classes.form} action={shareMeal}>
+                <form className={classes.form} action={formActions}>
                     <div className={classes.row}>
                         <p>
                             <label htmlFor="name">Your name</label>
-                            <input type="text" id="name" name="name" required />
+                            <input type="text" id="name" name="name" />
                         </p>
                         <p>
                             <label htmlFor="email">Your email</label>
-                            <input type="email" id="email" name="email" required />
+                            <input type="email" id="email" name="email" />
                         </p>
                     </div>
                     <p>
                         <label htmlFor="title">Title</label>
-                        <input type="text" id="title" name="title" required />
+                        <input type="text" id="title" name="title"  />
                     </p>
                     <p>
                         <label htmlFor="summary">Short Summary</label>
-                        <input type="text" id="summary" name="summary" required />
+                        <input type="text" id="summary" name="summary" />
                     </p>
                     <p>
                         <label htmlFor="instructions">Instructions</label>
@@ -42,8 +48,9 @@ export default function ShareMealPage() {
                         ></textarea>
                     </p>
                     <ImagePicker label='Your Image' name='image'/>
+                    {state.message && <p>{state.message}</p>}
                     <p className={classes.actions}>
-                        <button type="submit">Share Meal</button>
+                        <MealsFormSubmit/>
                     </p>
                 </form>
             </main>
---
+++ components/Meals/MealsFormSubmit.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+import {useFormStatus} from "react-dom";
+
+export function MealsFormSubmit() {
+    const {pending} = useFormStatus()
+
+    return <button type="submit" disabled={pending}>{pending ? 'Submitting...': 'Share Meal'}</button>
+}
